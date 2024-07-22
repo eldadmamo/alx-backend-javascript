9c55e2f3d9a4bf44112b3f6296c5f19648d540a4
eldadmamo <eldadf456@gmail.com>
2024-07-22 21:47:29 +0000

    alx-backend-javascript

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,7 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  for (let [index, value] of array.entries()) {
-    array[index] = appendString + value;
-  }
-
-  return array;
-}
