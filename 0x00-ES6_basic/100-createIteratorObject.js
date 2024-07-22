79f732507f34f37f1f7319d8e5c16ebe1fbc5447
eldadmamo <eldadf456@gmail.com>
2024-07-22 23:42:49 +0000

    0x00-ES6_basic

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function* createIteratorObject(report) {
+  const departments = Object.values(report.allEmployees);
+  for (const employees of departments) {
+    for (const employee of employees) {
+      yield employee;
+    }
+  }
+}
